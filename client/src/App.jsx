556020b6bf2436ac3cb5c4d78d74f0ab0d7b55e6
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-06-18 13:35:48 +0530

    Import error solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/home/Home"
 import Gigs from "./pages/gigs/Gigs"
